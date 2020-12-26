449683aa9db17009617ff8ce8fbe425b7139582a
Amaury Gonçalves Costa <baugoncalves@gmail.com>
2020-12-26 19:52:39 -0300

    Finishing last class for that module

--- tests/Feature/BasicCrudControllerTest.php
+++ tests/Feature/BasicCrudControllerTest.php
@@ -2,11 +2,14 @@
 
 namespace Tests\Feature;
 
+use App\Http\Controllers\BasicCrudController;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Tests\TestCase;
 use Tests\Stubs\Controllers\CategoryControllerStub;
 use Tests\Stubs\Models\CategoryStub;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
+use ReflectionClass;
 
 class BasicCrudControllerTest extends TestCase
 {
@@ -40,5 +43,39 @@ class BasicCrudControllerTest extends TestCase
             ->andReturn(['name' => '']);
 
         $this->controller->store($request);
+    }
+
+    public function testStore() {
+        $request = \Mockery::mock(Request::class);
+        $request
+            ->shouldReceive('all')
+            ->once()
+            ->andReturn(['name' => 'test_name', 'description' => 'description_test']);
+
+        $obj = $this->controller->store($request);
+        $this->assertEquals(
+            CategoryStub::find(1)->toArray(),
+            $obj->toArray()
+        );
+    }
+
+    public function testIfFindOrFailFetchModel() {
+        $category = CategoryStub::create(['name' => 'test_name', 'description' => 'test_description']);
+
+        $reflectionClass = new ReflectionClass(BasicCrudController::class);
+        $reflectionMethod = $reflectionClass->getMethod('findOrFail');
+        $reflectionMethod->setAccessible(true);
+
+        $result = $reflectionMethod->invokeArgs($this->controller, [$category->id]);
+        $this->assertInstanceOf(CategoryStub::class, $result);
+    }
+
+    public function testIfFindOrFailThrowExceptionWhenIdInvalid() {
+        $this->expectException(ModelNotFoundException::class);
+        $reflectionClass = new ReflectionClass(BasicCrudController::class);
+        $reflectionMethod = $reflectionClass->getMethod('findOrFail');
+        $reflectionMethod->setAccessible(true);
+
+        $result = $reflectionMethod->invokeArgs($this->controller, [0]);
     }
 }
