ab1550924b50fc5549c8106dcd2a9143bccc4d53
Amaury Gonçalves Costa <baugoncalves@gmail.com>
2021-05-09 17:06:42 -0300

    installing redux

---
+++ frontend/src/store/search/types.ts
@@ -0,0 +1,43 @@
+import {AnyAction} from 'redux';
+
+export interface Pagination {
+    page: number;
+    total: number;
+    per_page: number;
+}
+
+export interface Order {
+    sort: string | null;
+    dir: string | null;
+}
+
+export interface State {
+    search: any;
+    pagination: Pagination;
+    order: Order;
+}
+
+export interface SetSearchAction extends AnyAction {
+    payload: {
+        search: string | {value, [key: string]: any}
+    }
+}
+
+export interface SetPageAction extends AnyAction {
+    payload: {
+        page: number
+    }
+}
+
+export interface SetPerPageAction extends AnyAction {
+    payload: {
+        perPage: number
+    }
+}
+
+export interface SetOrderAction extends AnyAction {
+    payload: {
+        sort: string | null,
+        dir: string
+    }
+}
