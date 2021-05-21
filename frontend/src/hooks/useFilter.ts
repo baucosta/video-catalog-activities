8a9bd73d52ccf5bd59d9ccb2bf5a248e6e9b71fa
Amaury Gonçalves Costa <baugoncalves@gmail.com>
2021-05-21 08:45:26 -0300

    Mounting query string

--- frontend/src/hooks/useFilter.ts
+++ frontend/src/hooks/useFilter.ts
@@ -100,6 +100,36 @@ export class FilterManager {
         return newText;
     }
 
+    const schema = yup.Object().shape({
+        search: yup.string()
+            .transform(value => !value ? undefined : value)
+            .default(value: ''),
+        pagination: yup.object().shape({
+            page: yup.number()
+                .transform(value => isNaN(value) || parseInt(value) < 1 ? undefined : value)
+                .default(value: 1),
+            per_page: yup.number()
+                .oneOf([10, 15, 20])
+                .transform(value => isNaN(value) ? undefined : value)
+                .default(value: 15),
+        }),
+        order: yup.object().shape({
+            sort: yup.string()
+                .nullable()
+                .transform(value => {
+                    const columnsName = columns
+                        .filter(column => !column.options || column.options.sort !== false)
+                        .map(column => column.name);
+                    return columnsName.includes(value) ? value : undefined;
+                })
+                .default(value: null),
+            dir: yup.string()
+                .nullable()
+                .transform(value => !value || !['asc', 'desc'].includes(value.toLowerCase()) ? undefined : value)
+                .default(value: null),
+        })
+    })
+
     pushHistory() {
         const newLocation = {
             pathname: this.history.location.pathname,
