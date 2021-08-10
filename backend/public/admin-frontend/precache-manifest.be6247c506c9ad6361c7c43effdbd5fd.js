72d3a6ec979d09d2a66ee93e1b106b2f26a18c7a
Amaury Gonçalves Costa <baugoncalves@gmail.com>
2021-08-10 09:10:56 -0300

    Adding dusk

---
+++ backend/public/admin-frontend/precache-manifest.be6247c506c9ad6361c7c43effdbd5fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "072ead0494c0f4baaa052316c98f414b",
+    "url": "/admin-frontend/index.html"
+  },
+  {
+    "revision": "948a25e04cd311c21a96",
+    "url": "/admin-frontend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2d3f505641fcabd611db",
+    "url": "/admin-frontend/static/js/2.e1626c66.chunk.js"
+  },
+  {
+    "revision": "d683d2b75f7ae54c0951ec7ad07706e4",
+    "url": "/admin-frontend/static/js/2.e1626c66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "948a25e04cd311c21a96",
+    "url": "/admin-frontend/static/js/main.54f5c62b.chunk.js"
+  },
+  {
+    "revision": "9ab00dbf77a812511dbf",
+    "url": "/admin-frontend/static/js/runtime-main.3575b980.js"
+  }
+]);
